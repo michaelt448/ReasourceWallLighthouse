deb193327fd15cb2a321c8481265eee798f9353c
Jack Brennan <jack.brennan@nvngsystems.com>
2019-05-23 23:21:27 +0000

    commit file from resource routing  build

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,7 @@ $(() => {
   $.ajax({
     method: "GET",
     url: "/api/users"
+    //url: "/resource"
   }).done((users) => {
     for(user of users) {
       $("<div>").text(user.name).appendTo($("body"));
--- server.js
+++ server.js
@@ -17,6 +17,8 @@ const knexLogger  = require('knex-logger');
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
 
+//const resource = require("./routes/resource.js")
+
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
@@ -37,6 +39,7 @@ app.use(express.static("public"));
 
 // Mount all resource routes
 app.use("/api/users", usersRoutes(knex));
+//app.use("/resource", resource(knex));
 
 // Home page
 app.get("/", (req, res) => {
