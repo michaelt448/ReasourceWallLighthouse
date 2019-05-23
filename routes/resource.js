cfbe4a140f6e2d962286dbd9b716bd6c7c0d2fc7
Jack Brennan <jack.brennan@nvngsystems.com>
2019-05-23 20:04:54 +0000

    add changes to resource.js for knex

--- routes/resource.js
+++ routes/resource.js
@@ -1,1 +1,45 @@
-resource.js
+"use strict";
+
+const express = require('express');
+const router = express.Router();
+
+module.exports = (knex) => {
+
+    router.get("/", (req, res) => {
+        knex
+            .select("*")
+            .from("resources")
+            .then((results) => {
+                res.json(results);
+            });
+    });
+
+    return router;
+
+    router.get("/:id", (req, res) => {
+        knex
+            .select("*")
+            .from("resources")
+            .where("id = ?", req.params.id)
+            .then((results) => {
+                res.json(results);
+            });
+    });
+
+    return router;
+
+    // router.get("/:user_id/favourites", (req, res) => {
+    //     knex
+    //         .select("*")
+    //         .from("resources")
+    //         .where("id = ?", req.params.id)
+
+
+
+    //         .then((results) => {
+    //             res.json(results);
+    //         });
+    // });
+
+    return router;
+}
