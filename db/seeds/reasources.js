c6340f3b3018fb7448a033dc9bb558d2db549af0
vagrant <mikegithub77@gmail.com>
2019-05-23 18:51:03 +0000

    added likes seeds, does not work, seeing if commit is needed

--- db/seeds/reasources.js
+++ db/seeds/reasources.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('resources').insert({url: 'https://www.google.com/', title: 'Google', description: 'Used for searching new resources', user_id:'1', category:'research'}),
-        knex('resources').insert({url: 'https://lighthouselabs.ca/', title: 'Lighthouse', description: 'Main page for school', user_id:'1', category:'research'}),
-        knex('resources').insert({url: 'https://devhints.io/knex', title: 'Knex CheatSheet', description: 'Quick description of knex commands', user_id:'2', category:'knex'})
+        knex('resources').insert({id: '1', url: 'https://www.google.com/', title: 'Google', description: 'Used for searching new resources', user_id:'1', category:'research'}),
+        knex('resources').insert({id: '2', url: 'https://lighthouselabs.ca/', title: 'Lighthouse', description: 'Main page for school', user_id:'1', category:'research'}),
+        knex('resources').insert({id: '3', url: 'https://devhints.io/knex', title: 'Knex CheatSheet', description: 'Quick description of knex commands', user_id:'2', category:'knex'})
       ]);
     });
 };
