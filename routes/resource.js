17e36641f478eab5220a4fa7df711313b1e5da46
Jack Brennan <jack.brennan@nvngsystems.com>
2019-05-23 23:05:33 +0000

    new routing created in resource.js

--- routes/resource.js
+++ routes/resource.js
@@ -14,8 +14,6 @@ module.exports = (knex) => {
             });
     });
 
-    return router;
-
     router.get("/:id", (req, res) => {
         knex
             .select("*")
@@ -26,20 +24,28 @@ module.exports = (knex) => {
             });
     });
 
-    return router;
+    router.get("/:user_id/favourites", (req, res) => {
+        var subquery = knex('likes').where("id = ?", req.params.id)
+        knex
+            .select("*")
+            .from("resources")
+            .where("id = ?", req.params.id)
+            .orwhere('id', 'in', subquery)
+            .then((results) => {
+                res.json(results);
+            });
+    });
 
-    // router.get("/:user_id/favourites", (req, res) => {
-    //     knex
-    //         .select("*")
-    //         .from("resources")
-    //         .where("id = ?", req.params.id)
-
-
-
-    //         .then((results) => {
-    //             res.json(results);
-    //         });
-    // });
+    router.post('/:id/rank', (req, res) => {
+        const current_user = req.session.user_id;
+        knex('rank').insert(
+            {
+                user_id: current_user
+        resource_id: req.param.id
+        rank_value: req.body.rank_value
+            }
+        );
+    });
 
     return router;
 }
