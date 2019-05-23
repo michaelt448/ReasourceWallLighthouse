05519a69f95eb8ccf68f304e80d1fa72cf743374
vagrant <mikegithub77@gmail.com>
2019-05-23 21:07:17 +0000

    post and delete routes has been added

--- routes/resource.js
+++ routes/resource.js
@@ -1,1 +1,20 @@
-resource.js
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (knex) => {
+
+    router.post('/resources/:id/like', (req, res) => {
+        const current_user = req.session.user_id;
+        knex('likes').insert(
+            {user_id : current_user});
+    });
+
+    router.delete('resources/:id/like'), (req, res) => {
+        const current_user = req.session.user_id;
+        knex('likes').where({user_id : current_user}).del();
+    }
+
+
+    return router;
+  }
