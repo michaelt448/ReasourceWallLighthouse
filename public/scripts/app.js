e137d7bea74b3cb074f8a177052d87bb9b000c8c
vagrant <mikegithub77@gmail.com>
2019-05-25 19:21:46 +0000

    quick fix to bug to allow for login without duplication in the singleResource page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     const user_id = $('input');
 
     const renderPage = () => {
+      $('#anchor').empty();
       console.log('I am in render page');
       $.get('/api/resources/'+ 3) //---->>> 2 is resource id, hardcoded
       .then((object) => {
@@ -20,13 +21,15 @@ $(document).ready(function(){
 
     const renderUserSpecificPage = (resource) => {
       console.dir(resource);
-      renderComments(resource.comments); 
-      renderLikes(resource.likes);
-      renderRank(resource.ranks);
-      renderResources(resource.resourceProperties[0]);
-      checkPersonalLike(resource.personalLike);
-      checkPersonalRank(resource.renderPersonalRank);
-      renderCommentBox();
+      renderComments(resource.comments) 
+      renderLikes(resource.likes)
+      renderRank(resource.ranks)
+      renderResources(resource.resourceProperties[0])
+      checkPersonalLike(resource.personalLike)
+      checkPersonalRank(resource.renderPersonalRank)
+      renderCommentBox()
+      
+      
     }
     const renderComments = (comments) => {
       for(comment of comments) {
@@ -37,7 +40,7 @@ $(document).ready(function(){
 
     const renderCommentBox = () => {
       // very dirty code, this is to make the text box appear dynamically
-      $('.comments').append("<form class='commentBox'type = 'POST' action='/resources:id/comment'><textarea name='text' placeholder='Leave your thoughts below!!'></textarea><input type='submit' value='Comment'></form>")
+       $('.comments').append("<form class='commentBox'type = 'POST' action='/resources:id/comment'><textarea name='text' placeholder='Leave your thoughts below!!'></textarea><input type='submit' value='Comment'></form>")
     }
 
     const renderComment = (comment) => {
@@ -51,57 +54,75 @@ $(document).ready(function(){
     }
 
     const renderLikes = (like) => {
+      $('.totalLike').empty();
       const count = like[0].count;
-      $('.like').append($('<p>').text(count));
+      $('.totalLike').append($('<p>').text(count));
     }
 
     const renderRank = (rank) => {
+      $('.averageRank').empty();
+      console.log(rank[0].avg);
+      if(rank[0].avg === null) {
+         $('.averageRank').append($('<p>').text('No rank'));
+      }
+      else {
       const avg_rank = parseInt(rank[0].avg);
-      $('.rankBox').append($('<p>').text(avg_rank));
+       $('.averageRank').append($('<p>').text(avg_rank));
+      }
     }
 
     const renderResources = (properties) => {
-      console.dir(properties); 
-      $('div.properties')
+      // console.dir(properties); 
+      // const propertyTag = $('div').addClass('Properties');
+      $('div.properties').empty();
+       $('div.properties')
       .append(`<p> This is resource url : ${properties.url}</p>`)
       .append(`<p> This is when resource created : ${properties.create_at}</p>`)
       .append(`<p> This is resource title : ${properties.title}</p>`)
       .append(`<p> This is resource image : ${properties.url_img}</p>`)
       .append(`<p> This is resource description : ${properties.description}</p>`)
+      .addClass('properties');
+       
     }
 
-    const checkPersonalRank = (personRanks) => {
-      if(personRanks === undefined) {
-        return;
+    const checkPersonalRank = (personalRanks) => {
+      console.log(personalRanks)
+      $('.personalRank').empty();
+      if(personalRanks === undefined) {
+         $('.personalRank').append(`<p> NO PERSONAL RANK</p>`);
       } 
+      else {
       for(personalRank in personalRanks) {
         if(personalRank.user_id === Cookies.get('user_id')){
-          $('.personalRank').append(`<p>${personalRank.rank_value}</p>`);
-          return;
+           $('.personalRank').append(`<p>${personalRank.rank_value}</p>`);
+           return;
         }
       }
-      console.log(personRanks)
+       $('.personalRank').append(`<p> NO PERSONAL RANK</p>`);
     }
+  }
 
-    const checkPersonalLike = (personLikes) => {
+    const checkPersonalLike = (personalLikes) => {
+      $('personalLike').empty();
       if(personalLikes === undefined) {
-        return;
+         $('.personalLike').append('<p> NOT LIKED </p>');
       }
+      else {
       for(personalLike in personalLikes) {
         if(personalLike.user_id === Cookies.get('user_id')){
-          $('.personalLike').append('<p> LIKED </p>');
-          return;
+           $('.personalLike').append('<p> LIKED </p>');
         }
       } 
-      $('.personalLike').append('<p> NOT LIKED </p>');
-      console.log(personLikes);
+       $('.personalLike').append('<p> NOT LIKED </p>');
     }
+  }
 
     const renderUserPublicPage = (resource) => {
-      renderLikes(resource.likes);
-      renderRank(resource.ranks);
-      renderResources(resource.resourceProperties[0]);
-      renderComments(resource.comments); 
+      renderLikes(resource.likes)
+      renderRank(resource.ranks)
+      renderResources(resource.resourceProperties[0])
+      renderComments(resource.comments)
+      
     }
 
     likeButton.on('click', function(e) {
@@ -114,12 +135,10 @@ $(document).ready(function(){
       e.preventDefault();
       Cookies.set('user_id',user_id.val());
       renderPage();
-      // $.post('/api/resources/login',{user_id: user_id.val()}, () => {
-      // console.log('I posted a login');
-      // });
     }) 
 
     renderPage()
+    // $('#tweet-container').empty();
     // checkCookie();
     //Cookies.set()
     // renderComments([
